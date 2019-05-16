97d4039c1ad08b0a2c88c5fa49aadc55de1b6441
Estephan Rustom <steverustom@gmail.com>
2019-05-16 17:16:50 +0300

    constraints done

--- RayMessenger/ChatCell.swift
+++ RayMessenger/ChatCell.swift
@@ -22,14 +22,12 @@ class ChatCell: UITableViewCell {
         }
     }
 
-    func showOutgoingMessage(color: UIColor, text: String) {
+    private func showOutgoingMessage(color: UIColor, text: String) {
         let label =  UILabel()
         label.numberOfLines = 0
         label.font = UIFont.systemFont(ofSize: 18)
         label.textColor = .white
         label.text = text
-
-        print("FRAME minx: \(self.contentView.frame.minX), miny \(self.contentView.frame.minY), width: \(self.contentView.frame.width), height: \(self.contentView.frame.height)")
 
         let constraintRect = CGSize(width: 0.66 * self.contentView.frame.width, height: .greatestFiniteMagnitude)
         let boundingBox = text.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: label.font], context: nil)
@@ -41,17 +39,28 @@ class ChatCell: UITableViewCell {
 
         let bubbleImage = UIImage(named: "outgoing-message-bubble")?.resizableImage(withCapInsets: UIEdgeInsets(top: 17, left: 21, bottom: 17, right: 21), resizingMode: .stretch).withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
 
-        print("View minx: \(outgoingMessageView.frame.minX), miny \(outgoingMessageView.frame.minY), width: \(outgoingMessageView.frame.width), height: \(outgoingMessageView.frame.height)")
-
         outgoingMessageView.image = bubbleImage
         outgoingMessageView.tintColor = color
 
         self.contentView.addSubview(outgoingMessageView)
+        outgoingMessageView.translatesAutoresizingMaskIntoConstraints = false
+        print(self.contentView.frame.height - outgoingMessageView.frame.height)
+        outgoingMessageView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: (self.contentView.frame.height - outgoingMessageView.frame.height)/2).isActive = true
+        outgoingMessageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -(self.contentView.frame.height - outgoingMessageView.frame.height)/2).isActive = true
+        outgoingMessageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: (self.contentView.frame.width - outgoingMessageView.frame.width)-20).isActive = true
+        outgoingMessageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -20).isActive = true
+
+
         label.center = outgoingMessageView.center
         self.contentView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.topAnchor.constraint(equalTo: outgoingMessageView.topAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: outgoingMessageView.bottomAnchor).isActive = true
+        label.leadingAnchor.constraint(equalTo: outgoingMessageView.leadingAnchor, constant: 10).isActive = true
+        label.trailingAnchor.constraint(equalTo: outgoingMessageView.trailingAnchor, constant: 20).isActive = true
     }
 
-    func showIncomingMessage(color: UIColor, text: String) {
+    private func showIncomingMessage(color: UIColor, text: String) {
         let label =  UILabel()
         label.numberOfLines = 0
         label.font = UIFont.systemFont(ofSize: 18)
@@ -63,7 +72,7 @@ class ChatCell: UITableViewCell {
         label.frame.size = CGSize(width: ceil(boundingBox.width), height: ceil(boundingBox.height))
         let bubbleImageSize = CGSize(width: (label.frame.width) + 28, height: (label.frame.height) + 20)
 
-        let incomingMessageView = UIImageView(frame: CGRect(x: 20, y: self.contentView.frame.height - bubbleImageSize.height - 10, width: bubbleImageSize.width, height: bubbleImageSize.height))
+        let incomingMessageView = UIImageView(frame: CGRect(x: 0, y: self.contentView.frame.height - bubbleImageSize.height - 20, width: bubbleImageSize.width, height: bubbleImageSize.height))
 
         let bubbleImage = UIImage(named: "incoming-message-bubble")?.resizableImage(withCapInsets: UIEdgeInsets(top: 17, left: 21, bottom: 17, right: 21))
 
@@ -71,7 +80,19 @@ class ChatCell: UITableViewCell {
         incomingMessageView.tintColor = color
 
         self.contentView.addSubview(incomingMessageView)
+        incomingMessageView.translatesAutoresizingMaskIntoConstraints = false
+         print(self.contentView.frame.height - incomingMessageView.frame.height)
+        incomingMessageView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: (self.contentView.frame.height - incomingMessageView.frame.height)/2).isActive = true
+        incomingMessageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -(self.contentView.frame.height - incomingMessageView.frame.height)/2).isActive = true
+        incomingMessageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 20).isActive = true
+        incomingMessageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -(self.contentView.frame.width - incomingMessageView.frame.width)+20).isActive = true
+
         label.center = incomingMessageView.center
         self.contentView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.topAnchor.constraint(equalTo: incomingMessageView.topAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: incomingMessageView.bottomAnchor).isActive = true
+        label.leadingAnchor.constraint(equalTo: incomingMessageView.leadingAnchor, constant: 20).isActive = true
+        label.trailingAnchor.constraint(equalTo: incomingMessageView.trailingAnchor, constant: 10).isActive = true
     }
 }
