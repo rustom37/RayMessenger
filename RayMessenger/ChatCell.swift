ad8d2a023543bb4a1e8f76ff5bc0d43138acd4f6
Estephan Rustom <steverustom@gmail.com>
2019-05-24 13:05:21 +0300

    removed unnecessary files

--- RayMessenger/ChatCell.swift
+++ RayMessenger/ChatCell.swift
@@ -8,10 +8,6 @@
 
 import UIKit
 
-//protocol ChatCellDelegate {
-//    func hasPerformedSwipe(touch: CGPoint)
-//}
-
 class ChatCell: UITableViewCell {
 
     @IBOutlet var bubbleImage: UIImageView!
@@ -24,77 +20,14 @@ class ChatCell: UITableViewCell {
     fileprivate var incomingLeading: NSLayoutConstraint?
     fileprivate var incomingTrailing: NSLayoutConstraint?
 
-//    fileprivate var vibrancyView:UIVisualEffectView = UIVisualEffectView()
-//    fileprivate var vibrancySelectedBackgroundView:UIVisualEffectView = UIVisualEffectView()
-//    fileprivate var defaultSelectedBackgroundView:UIView?
-
-//    var delegate: ChatCellDelegate?
-//    var originalCenter = CGPoint()
-//    var isSwipeSuccessful = false
-//    var touch = CGPoint()
-
     override func awakeFromNib() {
         super.awakeFromNib()
         let longPress = UILongPressGestureRecognizer(target: self, action: #selector(selectAllOfLabel))
         message.isUserInteractionEnabled = true
         message.addGestureRecognizer(longPress)
 
-//        let pRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-//        pRecognizer.delegate = self
-//        addGestureRecognizer(pRecognizer)
-
         configureCell()
     }
-
-//    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-//        if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
-//            let translation = panGestureRecognizer.translation(in: superview!)
-//            if (abs(translation.x) > abs(translation.y)) && (translation.x < 0){
-//                touch = panGestureRecognizer.location(in: superview)
-//                return true
-//            }
-//            return false
-//        }else if gestureRecognizer is UITapGestureRecognizer {
-//            touch = gestureRecognizer.location(in: superview)
-//            return true
-//        }
-//        return false
-//    }
-//
-//    @objc func handlePan(_ recognizer: UIPanGestureRecognizer) {
-//        if recognizer.state == .began {
-//            originalCenter = center
-//        }
-//
-//        if recognizer.state == .changed {
-//            checkIfSwiped(recognizer: recognizer)
-//            delegate?.hasPerformedSwipe(touch: touch)
-//        }
-//
-//        if recognizer.state == .ended {
-//            let originalFrame = CGRect(x: 0, y: frame.origin.y, width: bounds.size.width, height: bounds.size.height)
-//            if isSwipeSuccessful{
-////                delegate?.hasPerformedSwipe(touch: touch)
-//                moveViewBackIntoPlaceSlowly(originalFrame: originalFrame)
-//            } else {
-//                moveViewBackIntoPlace(originalFrame: originalFrame)
-//            }
-//        }
-//    }
-//
-//    func checkIfSwiped(recognizer: UIPanGestureRecognizer) {
-//        let translation = recognizer.translation(in: self)
-//        center = CGPoint(x: originalCenter.x + max(translation.x, -20), y: originalCenter.y)
-//        isSwipeSuccessful = frame.origin.x < -frame.size.width / 3.0
-//    }
-//
-//    private func moveViewBackIntoPlace(originalFrame: CGRect) {
-//        UIView.animate(withDuration: 0.2, animations: {self.frame = originalFrame})
-//    }
-//
-//    private func moveViewBackIntoPlaceSlowly(originalFrame: CGRect) {
-//        UIView.animate(withDuration: 1.5, animations: {self.frame = originalFrame})
-//    }
 
     // MARK: - Cell Configuration
     fileprivate func configureCell() {
@@ -162,50 +95,6 @@ class ChatCell: UITableViewCell {
         let loc = recognizer.location(in: self.message)
         self.message.showMenu(location: loc)
     }
-
-//    // MARK: - Vibrance Effects
-//    open var blurEffectStyle: UIBlurEffect.Style? {
-//        didSet {
-//            updateBlur()
-//        }
-//    }
-//
-//    // For registering with UITableView without subclassing otherwise dequeuing instance of the cell causes an exception
-//    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//    }
-//
-//    required public init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//
-//        vibrancyView.frame = bounds
-//        vibrancyView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-//        for view in subviews {
-//            vibrancyView.contentView.addSubview(view)
-//        }
-//        addSubview(vibrancyView)
-//
-//        let blurSelectionEffect = UIBlurEffect(style: .light)
-//        vibrancySelectedBackgroundView.effect = blurSelectionEffect
-//        defaultSelectedBackgroundView = selectedBackgroundView
-//
-//        updateBlur()
-//    }
-//
-//    internal func updateBlur() {
-//        if let blurEffectStyle = blurEffectStyle, !UIAccessibility.isReduceTransparencyEnabled {
-//            let blurEffect = UIBlurEffect(style: blurEffectStyle)
-//            vibrancyView.effect = UIVibrancyEffect(blurEffect: blurEffect)
-//
-//            if selectedBackgroundView != nil && selectedBackgroundView != vibrancySelectedBackgroundView {
-//                vibrancySelectedBackgroundView.contentView.addSubview(selectedBackgroundView!)
-//                selectedBackgroundView = vibrancySelectedBackgroundView
-//            }
-//        } else {
-//            vibrancyView.effect = nil
-//            selectedBackgroundView = defaultSelectedBackgroundView
-//        }
-//    }
 }
 
 // MARK: - UILabel Extension
